472e4ec406f00750b78119154f6acd1e3b4cea6a
Anshul <anshulshakya5632@gmail.com>
2026-04-16 14:31:25 +0000

    Apr 16, 2026, 8:01 PM

--- backend/server.js
+++ backend/server.js
@@ -13,43 +13,36 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
-    credentials: true,
-  })
-);
-
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Nayamo API Running 💎");
+});
+
 // Routes
-app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/payment", paymentRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Nayamo API Running 💎");
-});
-
-// Start Server AFTER DB Connect
+// Start Server
 const startServer = async () => {
   try {
     await connectDB();
-
-    const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT} 🚀`);
     });
   } catch (error) {
-    console.error("Server start failed:", error.message);
-    process.exit(1);
+    console.log("Server Start Error ❌:", error.message);
   }
 };
 
