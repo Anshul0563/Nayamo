2d9d7de7e36b87cb418979b88da4ef4d455f3862
Anshul <anshulshakya5632@gmail.com>
2026-04-27 14:44:33 +0000

    Apr 27, 2026, 8:14 PM

--- admin/src/pages/AddProduct.jsx
+++ admin/src/pages/AddProduct.jsx
@@ -218,7 +218,7 @@ export default function AddProduct() {
               name="category"
               value={form.category}
               onChange={changeHandler}
-              placeholder="gold, silver, or diamond"
+placeholder="party, daily, traditional, western, statement, or bridal"
             />
           </div>
 
