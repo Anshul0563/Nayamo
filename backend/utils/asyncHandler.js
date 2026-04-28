612a37c3aacd9a3ba90bcf655b90b771696173ee
Anshul <anshulshakya5632@gmail.com>
2026-04-28 16:21:04 +0000

    Apr 28, 2026, 9:51 PM

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,10 +1,12 @@
 /**
  * Async Handler - Wraps async route handlers to catch errors
  * Eliminates need for try-catch in every controller
+ * Catches both sync throws and async rejections
  */
 const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+  Promise.resolve()
+    .then(() => fn(req, res, next))
+    .catch(next);
 };
 
 module.exports = asyncHandler;
-
