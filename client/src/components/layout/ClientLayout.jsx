f109f000953c9efb10ceef40c53bdc896f409173
Anshul <anshulshakya5632@gmail.com>
2026-05-06 03:21:50 +0000

    May 6, 2026, 8:51 AM

--- client/src/components/layout/ClientLayout.jsx
+++ client/src/components/layout/ClientLayout.jsx
@@ -6,19 +6,12 @@ import Footer from "./Footer";
 
 export default function ClientLayout() {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-[#070708] via-[#0A0A0C] to-[#070708] text-[#E8E8E8] overflow-hidden">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8 }}
-        className="relative"
-      >
+      <div className="min-h-screen flex flex-col bg-gradient-to-br from-[#070708] via-[#0A0A0C] to-[#070708] text-[#E8E8E8]">
         <Navbar />
-        <main className="flex-1 relative">
+        <main className="flex-1 relative pt-2">
           <Outlet />
         </main>
         <Footer />
-      </motion.div>
-    </div>
+      </div>
   );
 }
