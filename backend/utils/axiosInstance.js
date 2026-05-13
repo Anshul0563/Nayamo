c3016fcb12ccb18fb0c254ea2365764b5f1ab69d
Anshul <anshulshakya5632@gmail.com>
2026-05-13 07:12:51 +0000

    May 13, 2026, 12:42 PM

--- backend/utils/axiosInstance.js
+++ backend/utils/axiosInstance.js
@@ -1,14 +1,41 @@
 const axios = require("axios");
-const { getDelhiveryBaseUrl, getDelhiveryToken } = require("../config/env");
+const logger = require("../config/logger");
 
-const delhiveryToken = getDelhiveryToken();
+const {
+  getDelhiveryToken,
+  getDelhiveryBaseUrl,
+} = require("../config/env");
 
 const api = axios.create({
   baseURL: getDelhiveryBaseUrl(),
+  timeout: 15000,
   headers: {
-    ...(delhiveryToken ? { Authorization: `Token ${delhiveryToken}` } : {}),
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+    Authorization: `Token ${getDelhiveryToken()}`,
+  },
 });
+
+api.interceptors.request.use((config) => {
+  logger.info(
+    `DELHIVERY API => ${config.method?.toUpperCase()} ${config.url}`
+  );
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    logger.error(
+      `DELHIVERY ERROR => ${
+        error.response?.data
+          ? JSON.stringify(error.response.data)
+          : error.message
+      }`
+    );
+
+    return Promise.reject(error);
+  }
+);
 
 module.exports = api;
